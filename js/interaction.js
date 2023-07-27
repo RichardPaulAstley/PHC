0c366e8c5eff9f3ec8515ff445a68d04307c54b8
RichardPaulAstley <bobodu08@hotmail.fr>
2023-07-27 14:46:13 +0200

    Interaction Page Update
    Handling bonus in a better way.

--- js/interaction.js
+++ js/interaction.js
@@ -131,6 +131,8 @@ function generateDaycareEgg() {
 let lastClickTime = 0;
 let clickIntervals = [];
 
+// Manual Clicking 
+
 img.addEventListener("click", () => {
   const now = Date.now();
 
@@ -141,21 +143,6 @@ img.addEventListener("click", () => {
     return;
   }
   lastClickTime = now;
-
-  // Check for consistent clicking intervals
-  /*if (lastClickTime !== 0) {
-    clickIntervals.push(clickInterval);
-  }
-  if (clickIntervals.length >= 50) {
-    const firstInterval = clickIntervals[0];
-    for (let i = 1; i < clickIntervals.length; i++) {
-      if (Math.abs(clickIntervals[i] - firstInterval) > 10000) {
-        alert("Automated clicking detected. Please play the game manually.");
-        return;
-      }
-    }
-    clickIntervals.shift();
-  }*/
 
   let eggData = JSON.parse(localStorage.getItem("eggData") || "{}");
   if (!eggData.clicks) {
@@ -175,24 +162,23 @@ img.addEventListener("click", () => {
 
   team = team.map((member) => {
     if (member.isEgg) {
-      member.eggSteps += getRandomValue(18, 24);
-      if (new Date().getDay() === 5) { // Friday
-        member.eggSteps += getRandomValue(6, 8);
-      }
+      const eggStepsBase = getRandomValue(18, 24);
+      const eggStepsBonus = new Date().getDay() === 5 ? Math.round(eggStepsBase * 0.5) : 0; // Apply *1.5 bonus if it's Friday
+      member.eggSteps += eggStepsBase + eggStepsBonus;
     } else {
-      member.experience += getRandomValue(220, 320);
-      if (new Date().getDay() === 5) { // Friday
-        member.experience += getRandomValue(80, 107);
-      }
+      const experienceBase = getRandomValue(220, 320);
+      const experienceBonus = new Date().getDay() === 5 ? Math.round(experienceBase * 0.5) : 0; // Apply *1.5 bonus if it's Friday
+      member.experience += experienceBase + experienceBonus;
+      
       const pokemonData = pokemonDatabase.find(data => data.name === member.species);
       if (pokemonData) {
         const xpRequired = pokemonData.experience_group;
-
+  
         while (member.experience >= xpRequired && member.level < 100) {
           member.level++;
           member.experience -= xpRequired;
         }
-
+  
         if (member.level >= 100) {
           member.experience = xpRequired * 100;
         }
@@ -238,27 +224,25 @@ idleButton.addEventListener("click", () => {
       balance.pokeDollar += getRandomValue(1, 2);
       localStorage.setItem("balance", JSON.stringify(balance));
 
-      // Update team data as in the click event listener
       team = team.map((member) => {
         if (member.isEgg) {
-          member.eggSteps += getRandomValue(9, 12);
-          if (new Date().getDay() === 5) { // Friday
-            member.eggSteps += getRandomValue(3, 4);
-          }
+          const eggStepsBase = getRandomValue(9, 12);
+          const eggStepsBonus = new Date().getDay() === 5 ? Math.round(eggStepsBase * 0.5) : 0; // Apply *1.5 bonus if it's Friday
+          member.eggSteps += eggStepsBase + eggStepsBonus;
         } else {
-          member.experience += getRandomValue(110, 160);
-          if (new Date().getDay() === 5) { // Friday
-            member.experience += getRandomValue(37, 54);
-          }
+          const experienceBase = getRandomValue(110, 160);
+          const experienceBonus = new Date().getDay() === 5 ? Math.round(experienceBase * 0.5) : 0; // Apply *1.5 bonus if it's Friday
+          member.experience += experienceBase + experienceBonus;
+          
           const pokemonData = pokemonDatabase.find(data => data.name === member.species);
           if (pokemonData) {
             const xpRequired = pokemonData.experience_group;
-
+      
             while (member.experience >= xpRequired && member.level < 100) {
               member.level++;
               member.experience -= xpRequired;
             }
-
+      
             if (member.level >= 100) {
               member.experience = xpRequired * 100;
             }
@@ -280,3 +264,17 @@ idleButton.addEventListener("click", () => {
 
 img.src = getRandomPokemonOrEgg();
 
+// Function to get the bonus based on the day
+function getBonusMultiplier() {
+  return new Date().getDay() === 5 ? 1.5 : 1;
+}
+
+// Function to update the bonus alert content
+function updateBonusAlert() {
+  const bonusAlert = document.getElementById('bonus-alert');
+  const bonusMultiplier = getBonusMultiplier();
+  bonusAlert.textContent = `Today's bonus is : x${bonusMultiplier}`;
+}
+
+// Call the function to update the bonus alert when the page loads
+updateBonusAlert();
